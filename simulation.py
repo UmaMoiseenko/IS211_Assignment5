05dfc675c47afd23b4be79e429594a548a26a89f
Uma <uma.moiseenko@gmail.com>
2015-03-12 02:04:44 -0400

    still not working

--- simulation.py
+++ simulation.py
@@ -2,10 +2,40 @@ import argparse
 import csv
 import urllib2
 
-"""
-data file:
-http://s3.amazonaws.com/cuny-is211-spring2015/requests.csv
-"""
+class Server:
+    def __init__(self):
+        self.currentTask = None
+        self.timeRemaining = 0
+
+    def tick(self):
+        if self.currentTask != None:
+            self.timeRemaining = self.timeRemaining - 1
+            if self.timeRemaining <= 0:
+                self.currentTask = None
+
+    def busy(self):
+        if self.currentTask != None:
+            return True
+        else:
+            return False
+
+    def startNext(self,newreq):
+        self.currentTask = newreq
+        self.timeRemaining = newreq.getTime() #get seconds
+
+class Request:
+    def __init__(self,request):
+        self.timestamp = request[0]
+        self.timeleft = request[2]
+
+    def getStamp(self):
+        return self.timestamp
+
+    def getTime(self): #secs
+        return self.timeleft 
+
+    def waitTime(self, currenttime):
+        return currenttime - self.timestamp
 
 class Queue:
     def __init__(self):
@@ -20,68 +50,44 @@ class Queue:
     def dequeue(self):
         return self.items.pop()
 
+    def pop(self):
+        return self.items.pop(0)
+
     def size(self):
         return len(self.items)
 
-class Server:
-    def __init__(self, ppm):
-        self.pagerate = ppm
-        self.currentRequest = None
-        self.timeRemaining = 0
-
-    def tick(self):
-        if self.currentRequest != None:
-            self.timeRemaining = self.timeRemaining - 1
-            if self.timeRemaining <= 0:
-                self.currentRequest = None
-
-    def busy(self):
-        if self.currentRequest != None:
-            return True
-        else:
-            return False
-
-    def startNext(self,newRequest):
-        self.currentRequest = newRequest
-        self.timeRemaining = newRequest.getPages()
-
-class Request:
-    def __init__(self, time, timeReq):
-        self.time = time
-        self.pages = timeReq
-
-    def getStamp(self):
-        return self.timestamp
-
-    def getTimeReq(self):
-        return self.timeReq
-
-    def waitTime(self, currenttime):
-        return currenttime - self.timestamp
-
-
 def simulateOneServer(filename):
-    myServer = Server(0)
-    myQueue = Queue()
+    server = Server()
+    queue = Queue()
     waitingtimes = []
+    sortedReqests = {}
 
-    for row in filename:
-        numb_list.append(row[0])
-        request = Request(row[0], row[2])
-        myQueue.enqueue(request)
+    for request in filename:
+        newSec = int(request[0])
+        queue.enqueue(request)
 
-        if (not myServer.busy()) and (not myQueue.isEmpty()):
-            nextRequest = myQueue.dequeue()
-            waitingtimes.append(nextRequest.waitTime(currentSecond))
-            myServer.startNext(nextRequest)
+        if newSec in sortedReqests: 
+            sortedReqests[newSec].append(request)
+        else:
+            sortedReqests[newSec] = [request] 
 
-    myServer.tick()
-    averageWait=sum(waitingtimes)/len(waitingtimes)
-    print("Average Wait %6.2f secs %3d requests remaining."%(averageWait,myQueue.size()))
-    
+    for currentSecond in sortedReqests:
+        for req in sortedReqests[currentSecond]:
+            Request(req)
+            queue.dequeue()
+
+        if (not server.busy()) and (not queue.isEmpty()):
+            nextreq = Request(queue.dequeue())
+            waitingtimes.append(nextreq.waitTime(nextreq))
+            server.startNext(nextreq)
+
+        server.tick()
+
+    averageWait=sum(waitingtimes)/len(waitingtimes) * 0.001
+    print("Average Wait %2.2f secs for %3d requests."%(averageWait,queue.size()))
+
 
 def main():
-
     url_parser = argparse.ArgumentParser()
     url_parser.add_argument("--file", help='enter URL to CSV file', type=str)
     args = url_parser.parse_args()
@@ -90,6 +96,7 @@ def main():
         try:
             filename = csv.reader(urllib2.urlopen(args.file))
             simulateOneServer(filename)
+
         except:
             print "Invalid URL"
     else:
